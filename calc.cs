886abe668fd1d869ef7f24e3eaa6fe238bf427a5
David Folk <davidwfolk@gmail.com>
2020-07-27 09:27:37 -0600

    init

---
+++ calc.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
