145c9efbe1e38329598341d8d796c63a257f7bc4
David Folk <davidwfolk@gmail.com>
2020-07-27 09:28:38 -0600

    show that calculation works

--- calc.cs
+++ calc.cs
@@ -6,7 +6,11 @@ namespace Calc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
         }
     }
 }
